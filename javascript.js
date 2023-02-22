22c8bbcfde2672af4152db4b08b3a013d176778d
zekkenz <87358741+zekkenz@users.noreply.github.com>
2023-02-22 01:48:15 -0400

    random color
    first commit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function changeTitle(newTitle) {
+        document.querySelector("title").innerHTML = newTitle;
+      }
+    addEventListener("visibilitychange", e => {
+            if (e.target.visibilityState == "visible") {
+                    changeTitle("hola");
+                } else {
+                        changeTitle("vuelve");
+    }
+})
+
+let main = document.querySelector(".main");
+let boton = document.getElementById("boton");
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215.
+    let randomNumber = Math. random() * maxVal;
+    randomNumber = Math. floor(randomNumber);
+    randomNumber = randomNumber. toString(16);
+    let randColor = randomNumber. padStart(6, 0);
+    return `#${randColor. toUpperCase()}`
+}
+
+function proyect() {
+    main.style.backgroundColor= generateRandomColor();
+}
